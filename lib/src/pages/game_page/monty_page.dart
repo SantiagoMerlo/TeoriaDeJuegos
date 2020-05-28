817b101a039acb84b5972966203b1f8d14541268
SantiagoMerlo <santiago.agustin.merlo@gmail.com>
2020-05-28 14:01:14 -0300

    monty_page was edited

--- lib/src/pages/game_page/monty_page.dart
+++ lib/src/pages/game_page/monty_page.dart
@@ -6,52 +6,57 @@ class MontyPage extends StatefulWidget {
 }
 
 class _MontyPageState extends State<MontyPage> {
-
   Widget _state;
   int _action = 0;
   String _dirImage = 'assets/Game/';
-
-
-
+  int _iPart1 = 0;
 
   @override
   Widget build(BuildContext context) {
-
-
-    setState((){
-      if(_action == 0) _state = _part1();
-      else if(_action == 1) _state = _part2();
-      else if(_action == 2) _state = _part3();
-      else if(_action == 3) _state = _part4();
-      else _state = _part5();
+    setState(() {
+      if (_action == 0)
+        _state = _part1();
+      else if (_action == 1)
+        _state = _part2();
+      else if (_action == 2)
+        _state = _part3();
+      else if (_action == 3)
+        _state = _part4();
+      else
+        _state = _part5();
     });
 
-
-
-
     return Scaffold(
-       appBar: AppBar(
-         title: Text('Monty Hall')
-       ),
-       body: ListView(
-         children: <Widget>[
-           Stack(
-             children: <Widget>[
-              Container(
-                height: double.maxFinite,
-                width: double.maxFinite,
-                child: Image(image: AssetImage('assets/back.png'),
-                fit: BoxFit.cover,)),
-               _state,
-             ],
-           )
-           ]),
+      appBar: AppBar(title: Text('Monty Hall')),
+      body: Stack(children: <Widget>[
+        Container(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            child: Image(
+              image: AssetImage('assets/back.png'),
+              fit: BoxFit.cover,
+            )),
+        ListView(
+          children: <Widget>[
+            _state,
+          ],
+        )
+      ]),
     );
   }
 
 //introduccion - presentacion
-  Widget _part1(){
+  Widget _part1() {
     String image = _dirImage + 'presenter.png';
+
+    String texto;
+
+    if(_iPart1 == 0) texto = '\n\n\tBienvenidos queridos televidentes';
+    if(_iPart1 == 1) texto = '\tSoy su presentador, Monty Hall, y en el dia de hoy tendremos un nuevo participante de este increbile show!';
+    if(_iPart1 == 2) texto = '\tLas reglas son muy simples!';
+    if(_iPart1 == 3) texto = '\tVas a tener que elegir una de tres puertas';
+    if(_iPart1 == 4) texto = '\tSi la puerta es la correcta, te ganaras una moto!\n\nCaso contrario una cabra';
+    if(_iPart1 == 5) texto = '\tMucha suerte!';
 
     return Container(
       child: Column(
@@ -61,42 +66,58 @@ class _MontyPageState extends State<MontyPage> {
             alignment: AlignmentDirectional.bottomEnd,
             children: <Widget>[
               Container(
-                height: 325,
-                width: double.maxFinite,
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 50),
-                    _cuadroTexto(),
-                    SizedBox(height: 30)
-                  ],
-                )),
+                  height: 325,
+                  width: double.maxFinite,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 50),
+                      _cuadroTexto(
+                          texto),
+                      SizedBox(height: 30)
+                    ],
+                  )),
               Container(
-                height: 150,
-                width: 200,
-                child: Image(image: AssetImage(image), fit: BoxFit.contain,)),
+                  height: 150,
+                  width: 200,
+                  child: Image(
+                    image: AssetImage(image),
+                    fit: BoxFit.contain,
+                  )),
             ],
-          ),          
-          SizedBox(height: 20,),
+          ),
+          SizedBox(
+            height: 20,
+          ),
           _botton()
         ],
       ),
     );
   }
 
-  Widget _botton(){
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Container(
-        height: 50,
-        width: 200,
-        color: Theme.of(context).primaryColor,
-        child: Center(
-          child: Text('Continuar', style: Theme.of(context).textTheme.subhead),
+  Widget _botton() {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _iPart1++;
+          if(_iPart1 > 5) _action++;
+        });
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          height: 50,
+          width: 200,
+          color: Theme.of(context).primaryColor,
+          child: Center(
+            child:
+                Text('Continuar', style: Theme.of(context).textTheme.subhead),
+          ),
         ),
       ),
     );
   }
-  Widget _cuadroTexto(){
+
+  Widget _cuadroTexto(String texto) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Container(
@@ -104,10 +125,18 @@ class _MontyPageState extends State<MontyPage> {
         width: 250,
         color: Theme.of(context).secondaryHeaderColor,
         child: Container(
-          margin: EdgeInsets.all(10),
-          child: Text('Hola queridos televidentes. \nSoy su presentador, Monty Hall, y en el dia de hoy tendremos un nuevo participante de este increbile show!\n\n...',
-          textAlign: TextAlign.justify,
-          style: Theme.of(context).textTheme.button,
+          margin: EdgeInsets.symmetric(vertical: 30 , horizontal: 15),
+          child: Column(
+            children: <Widget>[
+              Text(
+                texto,
+                textAlign: TextAlign.justify,
+                style: Theme.of(context).textTheme.button,
+              ),
+              SizedBox(height: 15),
+              Center(
+                child: Text('...', style: Theme.of(context).textTheme.button))
+            ],
           ),
         ),
       ),
@@ -115,25 +144,96 @@ class _MontyPageState extends State<MontyPage> {
   }
 
 // elegir primera opcion
-  Widget _part2(){
-    return Container(
-      child: Text('2')
+  Widget _part2() {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 50),
+        Stack(
+          children: <Widget>[
+            Container(
+              width: double.maxFinite,
+              height: 150,
+              child: Image(
+                image: AssetImage('assets/Game/3doorGroup.png'),
+                fit: BoxFit.cover,
+              )
+            ),
+            Container(
+              padding: EdgeInsets.only(top:100,left: 200),
+              width: 350,
+              height: 300,
+              child: Image(
+                  image: AssetImage('assets/Game/presenterTable.png'),
+                  fit: BoxFit.cover,
+               ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top:225, left: 20),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    height: 50,
+                    width: 150,
+                    color: Theme.of(context).secondaryHeaderColor,
+                    child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 15),
+                          Text(
+                            'Elige una puerta!',
+                            textAlign: TextAlign.justify,
+                            style: Theme.of(context).textTheme.button,
+                          ),
+                          SizedBox(height: 15),
+                        ],
+                    ),
+                  ),
+                ),
+              )
+          ],
+        ),
+        SizedBox(height: 50),
+        _bottonOpcion(1, 50 , 300),
+        SizedBox(height: 15),
+        _bottonOpcion(2 , 50 , 300),
+        SizedBox(height:15),
+        _bottonOpcion(3 , 50 , 300),
+        SizedBox(height: 15)
+      ],
+    );
+  }
+  Widget _bottonOpcion(int opc, double height, double width){
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+        });
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          height: height,
+          width: width,
+          color: Theme.of(context).primaryColor,
+          child: Center(
+            child:
+                Text('Puerta ' + opc.toString(), style: Theme.of(context).textTheme.subhead),
+          ),
+        ),
+      ),
     );
   }
 
 // texto de apertura
-  Widget _part3(){
+  Widget _part3() {
     return Container();
   }
 
 // eleccion nuevamente
-  Widget _part4(){
+  Widget _part4() {
     return Container();
   }
 
 // resultado
-  Widget _part5(){
+  Widget _part5() {
     return Container();
   }
-
 }
